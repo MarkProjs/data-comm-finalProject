15695858c08e4dee858ae9dc96bd267628e6b35f
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-11-23 21:00:47 -0500

    adding the senseLED workin in the circuit and the code

--- FinalProject/src/main/Python/SenseLED.py
+++ FinalProject/src/main/Python/SenseLED.py
@@ -7,19 +7,32 @@
 ########################################################################
 import RPi.GPIO as GPIO
 
-ledPin = 12       # define ledPin--> to be changed
-sensorPin = 13    # define sensorPin--> to be changed
+ledPin = 12       # define ledPin
+sensorPin = 13    # define sensorPin
 
-GPIO.setmode(GPIO.BOARD)        # use PHYSICAL GPIO Numbering
-GPIO.setup(ledPin, GPIO.OUT)    # set ledPin to OUTPUT mode
-GPIO.setup(sensorPin, GPIO.IN)  # set sensorPin to INPUT mode
+def setup():
+    GPIO.setmode(GPIO.BOARD)        # use PHYSICAL GPIO Numbering
+    GPIO.setup(ledPin, GPIO.OUT)    # set ledPin to OUTPUT mode
+    GPIO.setup(sensorPin, GPIO.IN)  # set sensorPin to INPUT mode
 
-if GPIO.input(sensorPin)==GPIO.HIGH:
-    GPIO.output(ledPin,GPIO.HIGH) # turn on led
-    print ('led turned on >>>')
-else :
-    GPIO.output(ledPin,GPIO.LOW) # turn off led
-    print ('led turned off <<<')
+def loop():
+    while True:
+        if GPIO.input(sensorPin)==GPIO.HIGH:
+            GPIO.output(ledPin,GPIO.HIGH) # turn on led
+            print ('led turned on >>>')
+        else :
+            GPIO.output(ledPin,GPIO.LOW) # turn off led
+            print ('led turned off <<<')
 
-GPIO.cleanup() # Release GPIO resource
+def destroy():
+    GPIO.cleanup()                     # Release GPIO resource
+
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
+
 
