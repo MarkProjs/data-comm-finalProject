2b8529ec93a2b4c1ea5c46b8f208ddc74e242eb5
Mark Agluba <mark.agluba@dawsoncollege.qc.ca>
2022-12-04 00:29:03 -0500

    change the outpath on where to save the image saved

--- FinalProject/src/main/java/com/mycompany/finalproject/CameraApp.java
+++ FinalProject/src/main/java/com/mycompany/finalproject/CameraApp.java
@@ -29,7 +29,7 @@ public class CameraApp implements Application {
         
         //Configure the camera setup
         var config = Camera.PicConfig.Builder.newInstance()
-                .outputPath("/home/markisawesome/Pictures/")
+                .outputPath("/home/markisawesome/NetBeansProject/data-comm-final-project/FinalProject/src/main/resources/images/")
 		.delay(3000)
 		.disablePreview(true)
 		.encoding(Camera.PicEncoding.PNG)
