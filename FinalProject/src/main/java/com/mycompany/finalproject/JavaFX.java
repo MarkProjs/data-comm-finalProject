c08d1a3243791e44ba058c9a3b6a2ce727ce8bec
unknown <jeremy.tang@dawsoncollege.qc.ca>
2022-12-06 20:57:30 -0500

    finish keystore prompt

--- FinalProject/src/main/java/com/mycompany/finalproject/JavaFX.java
+++ FinalProject/src/main/java/com/mycompany/finalproject/JavaFX.java
@@ -97,9 +97,9 @@ public class JavaFX extends HBox {
         grid.add(actiontarget, 1, 6);
 
         btn.setOnAction((e) -> {
+        actiontarget.setFill(Color.BLACK);
+        actiontarget.setText("Signing in...");
             try {
-                actiontarget.setFill(Color.BLACK);
-                actiontarget.setText("Signing in...");
                 this.mqtt = new MyMqtt(userTextField.getText(), pwBox.getText());
                 this.mqtt.connectClient();
                 Alert alert = new Alert(AlertType.INFORMATION);
@@ -126,7 +126,52 @@ public class JavaFX extends HBox {
     }
 
     private void buildKeystorePrompt() {
-        
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Credentials for Keystore");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userPath = new Label("File Path:");
+        grid.add(userPath, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Load");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        btn.setOnAction((e) -> {
+                try {
+                        Alert alert = new Alert(AlertType.INFORMATION);
+                        alert.setTitle("Information Dialog");
+                        alert.setHeaderText(null);
+                        alert.setContentText("message");
+                        alert.showAndWait();
+                        this.buildScreen();    
+                        this.threads.startDHTThread(markHumidTile, markTempTile);
+                        this.threads.startDoorBellThread(markDoorBellTile);
+                        this.threads.startSenseLEDThread(markSensorTile, markImageTile);
+                } catch (Exception exc) {
+                        Logger.getLogger(JavaFX.class.getName()).log(Level.SEVERE, null, exc);
+                        System.out.println(exc);
+                        userTextField.clear();
+                        pwBox.clear();
+                }
+        });
     }
 
     private void buildScreen() throws IOException {
